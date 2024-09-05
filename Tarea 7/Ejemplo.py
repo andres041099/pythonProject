3106d5bed2163d451a0db2c13809c8785ebadc9a
andres041099 <andresrodriguezliberato1@gmail.com>
2024-09-05 14:18:21 -0400

    Pograma Ejemplo de esta archivo

---
+++ Tarea 7/Ejemplo.py
@@ -0,0 +1,24 @@
+#lista
+#listas(list):
+# numeros= [1,2,3,4,5,6,7,8,9,10]
+# cadenas_caracteres = ["jose","Maria","Pedro"]
+# varios_datos=["maria",9,2,5,True,False]
+# #Tuplas(tuplas):
+# colores=("Rojo","Amarillo","verde","Azul")
+#
+# numeros.append(23)
+# # print(numeros)
+#
+# numeros.remove(10)
+# print("se removio a: ",10)
+# print(numeros)
+#
+# edades={"Ana":22,"Pedro":31,"Carlos":20}
+# print(edades["Ana"])
+# for e in edades:
+#     print(e)
+# for m in edades.keys():
+#     print(m)
+# for n in edades.values():
+#     print(n)
+# for i in  edades.items():
